42d18695cac70eb7a565236313572c8a0c7f8df1
Doncho Penev <b.d.penev@gmail.com>
2019-03-11 16:09:28 +0100

    creating model restaurant and category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_144616) do
+ActiveRecord::Schema.define(version: 2019_03_11_150824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "restaurant_category"
+    t.text "description"
+    t.integer "opening_hour"
+    t.integer "closing_hour"
+    t.string "picture"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
