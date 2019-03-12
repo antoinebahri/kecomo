471cb8eb72d198259bfae793eada38300839a4e1
Doncho Penev <b.d.penev@gmail.com>
2019-03-12 11:38:55 +0100

    correcting git

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,21 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    @rest_by_cat_by_city = Restaurant.search_by_cat(params[:category].name).where(params[:city])
+    raise
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # def new
+  #   @restaurant = Restaurant.new
+  # # end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+  end
+
+  private
+end
---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant < ApplicationRecord
+  has_many :meals
+  has_many :awards, through: :meals
+  belongs_to :user, optional: true
+  validates :name, presence: true, uniqueness: { scope: :address }
+  validates :city, presence: true
+
+  include PgSearch
+  pg_search_scope :search_by_cat, against: :category
+end
