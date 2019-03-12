97f2eb86fa489d7c6268d019eb611e863982fbff
Antoine Bahri <antoinebahri@gmail.com>
2019-03-12 18:59:14 +0100

    Seed from api fully functional / 50 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,34 @@
 
 require 'json'
 require 'open-uri'
+require "httparty"
 
-client_id = VGQ5PRPHTBC33UG5AO1XMGNMO1TAWZ32JCUN1HIVF4UW2QGJ
-client_secret = ZT5N4QXUKB3TFNRP5TZSAEI1DOLQVE333SNAVR5PFHTISKDD
+puts "Destroying all restaurants"
+Restaurant.destroy_all
 
-url = "https://api.foursquare.com/v2/venues/explore?client_id=#{client_id}&client_secret=#{client_secret}&v=20180323&near=barcelona&query=restaurants"
+url = "https://api.foursquare.com/v2/venues/explore?client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_SECRET_ID"]}&v=20180323&near=barcelona&query=restaurants&limit=50"
 
-places_serialized = open(url).read
+places_serialized = HTTParty.get(url).body
 places_list = JSON.parse(places_serialized)
 
+puts "Creating restaurants"
+places_list["response"]["groups"][0]["items"].each do |place|
+  url_photo = "https://api.foursquare.com/v2/venues/#{place["venue"]["id"]}/photos?client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_SECRET_ID"]}&v=20180323"
 
-places_list[:response][:groups][0][:items].each do |place|
-  url_photo = "https://api.foursquare.com/v2/venues/#{place[:venue][:id]}/photos?client_id=client_id&client_secret=#{client_secret}&v=20180323"
-
-  photo_serialized = open(url).read
+  photo_serialized = HTTParty.get(url_photo).body
+  # open(url_photo).read
   photo_json = JSON.parse(photo_serialized)
 
-  Restaurant.new(name: place[:venue][:name], address: place[:venue][:location][:address], city: place[:venue][:location][:city], category: place[:venue][:categories][0][:name], picture: "#{photo_serialized[:response][:photos][:items][0][:prefix]}/500x500/#{photo_serialized[:response][:photos][:items][0][:suffix]}", longitude: place[:venue][:location][:lng], place[:venue][:location][:lat])
-
+  resto = Restaurant.create!(
+    name: place["venue"]["name"],
+    address: place["venue"]["location"]["address"],
+    city: place["venue"]["location"]["city"],
+    restaurant_category: place["venue"]["categories"][0]["name"],
+    longitude: place["venue"]["location"]["lng"],
+    latitude: place["venue"]["location"]["lat"]
+  )
+  if !photo_json["meta"].keys.include?("errorType")
+    resto.picture = "#{photo_json["response"]["photos"]["items"][0]["prefix"]}500x500#{photo_json["response"]["photos"]["items"][0]["suffix"]}"
+    resto.save
+  end
 end
