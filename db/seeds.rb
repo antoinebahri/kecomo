8cd33ff0aebec10ccd24712196a2e692b092ee07
Antoine Bahri <antoinebahri@gmail.com>
2019-03-12 16:02:44 +0100

    Add the seed file. Seed file need to be tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+client_id = VGQ5PRPHTBC33UG5AO1XMGNMO1TAWZ32JCUN1HIVF4UW2QGJ
+client_secret = ZT5N4QXUKB3TFNRP5TZSAEI1DOLQVE333SNAVR5PFHTISKDD
+
+url = "https://api.foursquare.com/v2/venues/explore?client_id=#{client_id}&client_secret=#{client_secret}&v=20180323&near=barcelona&query=restaurants"
+
+places_serialized = open(url).read
+places_list = JSON.parse(places_serialized)
+
+
+places_list[:response][:groups][0][:items].each do |place|
+  url_photo = "https://api.foursquare.com/v2/venues/#{place[:venue][:id]}/photos?client_id=client_id&client_secret=#{client_secret}&v=20180323"
+
+  photo_serialized = open(url).read
+  photo_json = JSON.parse(photo_serialized)
+
+  Restaurant.new(name: place[:venue][:name], address: place[:venue][:location][:address], city: place[:venue][:location][:city], category: place[:venue][:categories][0][:name], picture: "#{photo_serialized[:response][:photos][:items][0][:prefix]}/500x500/#{photo_serialized[:response][:photos][:items][0][:suffix]}", longitude: place[:venue][:location][:lng], place[:venue][:location][:lat])
+
+end
