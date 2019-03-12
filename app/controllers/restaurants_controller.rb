74b3f3d509b8787bc5e3c59c25990cc01c335e8b
Nicolas Gil Michel <ngilmichel@gmail.com>
2019-03-12 16:18:00 +0100

    add controllers. not all. meals_controller showing all meals within a certain category. quick seed made.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,26 +1,38 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
-    @rest_by_cat_by_city = Restaurant.search_by_cat(params[:category].name).where(params[:city])
-    raise
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
   end
 
-  # def new
-  #   @restaurant = Restaurant.new
-  # # end
+  def new
+    @restaurant = Restaurant.new
+  end
 
   def create
     @restaurant = Restaurant.create(restaurant_params)
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.update(restaurant_params)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to root_path
+  end
+
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :city)
+    params.require(:restaurant).permit(:name, :address, :city, :restaurant_category, :description, :opening_hour, :closing_hour, :picture, :latitude, :longitude)
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,69 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Deleting seeds...'
+Award.delete_all
+Meal.delete_all
+Category.delete_all
+Restaurant.delete_all
+
+puts "Creating empty arrays"
+user_index_array = []
+cat_index_array = []
+rest_index_array = []
+meal_index_array = []
+award_index_array = []
+
+puts 'Creating categories...'
+  Category.create(name: 'Burger');
+  Category.create(name: 'Pizza');
+  Category.create(name: 'Noodles');
+puts 'Done'
+
+puts 'Seeding empty array of categories with their indices'
+Category.all.each do |cat|
+  cat_index_array << cat.id
+end
+
+puts 'Creating restaurants...'
+  Restaurant.create(name: 'Bacoa', address: 'Rambla 123', city: 'Barcelona', restaurant_category: 'American', description: 'Dope');
+  Restaurant.create(name: 'Timesburg', address: 'Rambla 456', city: 'Barcelona', restaurant_category: 'American', description: 'Cool');
+  Restaurant.create(name: 'McDonalds', address: 'Rambla 789', city: 'Barcelona', restaurant_category: 'American', description: 'Ass');
+puts 'Done'
+
+puts 'Seeding empty array of restaurants with their indices'
+Restaurant.all.each do |restaurant|
+  rest_index_array << restaurant.id
+end
+
+puts 'Creating meals...'
+  Meal.create(name: 'Chorizo Burger', description: 'Amazing', restaurant_id: rest_index_array[0], category_id: cat_index_array[0]);
+  Meal.create(name: 'Picanha Burger', description: 'Great', restaurant_id: rest_index_array[1], category_id: cat_index_array[0]);
+  Meal.create(name: 'Big Mac', description: 'Plastic', restaurant_id: rest_index_array[2], category_id: cat_index_array[0]);
+puts 'Done'
+
+puts 'Seeding empty array of meals with their indices'
+Meal.all.each do |meal|
+  meal_index_array << meal.id
+end
+
+puts 'Creating users...'
+  User.create(first_name: 'Juan', last_name: 'Goycochea', email: 'jgoyco@gmail.com', password: '123456');
+  User.create(first_name: 'Alberto', last_name: 'Goycochea', email: 'agoyco@gmail.com', password: '123456');
+  User.create(first_name: 'Luis', last_name: 'Goycochea', email: 'lgoyco@gmail.com', password: '123456');
+puts 'Done'
+
+puts 'Seeding empty array of users with their indices'
+User.all.each do |user|
+  user_index_array << user.id
+end
+
+puts 'Creating awards...'
+  Award.create(review: 'Boom', meal_id: meal_index_array[0], category_id: cat_index_array[0], user_id: user_index_array[0]);
+  Award.create(review: 'Boom', meal_id: meal_index_array[0], category_id: cat_index_array[0], user_id: user_index_array[0]);
+  Award.create(review: 'Boom', meal_id: meal_index_array[0], category_id: cat_index_array[0], user_id: user_index_array[0]);
+  Award.create(review: 'Boo Yah', meal_id: meal_index_array[1], category_id: cat_index_array[0], user_id: user_index_array[1]);
+  Award.create(review: 'Boo Yah', meal_id: meal_index_array[1], category_id: cat_index_array[0], user_id: user_index_array[1]);
+  Award.create(review: 'Bullshit', meal_id: meal_index_array[2], category_id: cat_index_array[0], user_id: user_index_array[2]);
+puts 'Done'
