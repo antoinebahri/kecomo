18ab209bb3ff855730175dc6e1c080bf31d09388
Doncho Penev <b.d.penev@gmail.com>
2019-03-14 00:23:43 +0100

    routes cleaned :)

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
   resources :awards, except: [:show]
 
   resources :categories, only: [:show] do
-    get '/meals', to: 'meals#index'
   end
-
-  get '/meals/:id', to: 'meals#show', as: 'meal_path'
-  get '/meals', to: 'meals#index', as: 'meals_path'
 
   resources :restaurants do
     resources :meals
