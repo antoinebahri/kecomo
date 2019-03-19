88dbc5027d6c5a877842870617ceafc432f49529
Doncho Penev <b.d.penev@gmail.com>
2019-03-19 12:22:41 +0100

    award only one meal in category implemented

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -19,9 +19,10 @@ class MealsController < ApplicationController
           end
           flattened_array = all_results_array.flatten
           sorted_array = flattened_array.sort_by {|meal| meal.awards.count}
-          @meals = sorted_array.reverse!
+          @meals = sorted_array.reverse!.uniq
         end
       end
+      # raise
     elsif params[:category_id].present?
       @meals = Meal.all.where(category_id: params[:category_id]).first(10)
       @meals.sort_by! do |meal|
@@ -33,11 +34,12 @@ class MealsController < ApplicationController
     end
     # raise
     @current_user_awarded_categories = []
-    current_user_awards = current_user.awards
-    current_user_awards.each do |award|
+    @current_user_awards = current_user.awards
+    @current_user_awards.each do |award|
       @current_user_awarded_categories << award.meal.category
     end
     @current_user_awarded_categories = @current_user_awarded_categories.sort
+    # raise
   end
 
   def show
